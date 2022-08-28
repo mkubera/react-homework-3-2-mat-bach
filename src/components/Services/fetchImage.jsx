2e0416770b28dfafca2c3208a06b89a93d0a5412
MateuszBachor <mateuszbachor3@gmail.com>
2022-08-28 06:31:04 +0200

    fetch

--- src/components/Services/fetchImage.jsx
+++ src/components/Services/fetchImage.jsx
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const fetchImage = async (query, page) => {
+  const baseUrl = 'https://pixabay.com/api';
   try {
     const response = await axios.get(
-      `https://pixabay.com/api?key=28406091-8008b7c1afae3beb3d4e940a7&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=12}`
+      `${baseUrl}?key=28406091-8008b7c1afae3beb3d4e940a7&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=12}`
     );
     return response.data;
   } catch (error) {
