acd02a6903da0b280f3b85276b813d9b2a9443be
Mikolaj Kubera <rainteller@tutanota.de>
2022-08-29 22:15:48 +0200

    Update fetchImage.jsx

--- src/components/Services/fetchImage.jsx
+++ src/components/Services/fetchImage.jsx
@@ -3,7 +3,9 @@
 const fetchImage = async (query, page) => {
 //  const baseUrl = 'https://pixabay.com/api';
   const url = `https://pixabay.com/api?key=28406091-8008b7c1afae3beb3d4e940a7&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=12`;
-//   try {
+  console.log(url);
+  console.log("https://pixabay.com");
+  //   try {
 //     const response = await axios.get(url);
 //     return response.data;
     const resp = await fetch(url);
