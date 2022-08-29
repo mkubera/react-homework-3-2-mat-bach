3d318db7d2c67b79ba380df7589196e92a49530a
Mikolaj Kubera <rainteller@tutanota.de>
2022-08-29 21:58:30 +0200

    Update fetchImage.jsx

--- src/components/Services/fetchImage.jsx
+++ src/components/Services/fetchImage.jsx
@@ -4,7 +4,7 @@ const fetchImage = async (query, page) => {
 //  const baseUrl = 'https://pixabay.com/api';
   try {
     const response = await axios.get(
-      `https://pixabay.com/api?key=28406091-8008b7c1afae3beb3d4e940a7&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=12}`
+      `https://pixabay.com/api?key=28406091-8008b7c1afae3beb3d4e940a7&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=12`
     );
     return response.data;
   } catch (error) {
